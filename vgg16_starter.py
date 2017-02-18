58b0e88122494465bc38b17ae44e8e49ac7599d3
MultiLi <litian9308@gmail.com>
2017-02-18 12:39:57 -0800

    bonus

--- vgg16_starter.py
+++ vgg16_starter.py
@@ -1,7 +1,7 @@
 import keras
 from keras.applications import VGG16
 from keras.models import Model
-from keras.layers import Dense
+from keras.layers import Flatten, Dense
 
 
 def getModel( output_dim ):
@@ -12,16 +12,26 @@ def getModel( output_dim ):
     '''
     vgg_model = VGG16( weights='imagenet', include_top=True )
     vgg_out = vgg_model.layers[-2].output #Last FC layer's output
+    cv5_out = vgg_model.layers[-6].output
+    cv4_out = vgg_model.layers[-10].output
+
+    x5 = Flatten(name='flatten5')(cv5_out)
+    x4 = Flatten(name='flatten4')(cv4_out)
+
     for layer in vgg_model.layers:
         layer.trainable = False
     softmax_layer = Dense(output_dim , init = 'normal', activation = 'softmax')(vgg_out)     #Create softmax layer taking input as vgg_out
-    #Create new transfer learning model
+    softmax_5 = Dense(output_dim , init = 'normal', activation = 'softmax')(x5)
+    softmax_4 = Dense(output_dim , init = 'normal', activation = 'softmax')(x4)
+
     tl_model = Model(input=vgg_model.input, output=softmax_layer)
+    tl_model5 = Model(input=vgg_model.input, output=softmax_5)
+    tl_model4 = Model(input=vgg_model.input, output=softmax_4)
 
     #Freeze all layers of VGG16 and Compile the model
     #Confirm the model is appropriate
 
-    return tl_model
+    return tl_model, tl_model5, tl_model4
 
 if __name__ == '__main__':
     #Output dim for your dataset
